9aaedb347d0b4e744e243d4fd4786f39219f76cc
Banfi Peter <p6t6rbanfi@gmail.com>
2018-02-24 22:42:05 +0100

    some minor and major changes

--- JS/adminP/users.js
+++ JS/adminP/users.js
@@ -14,6 +14,7 @@ function successAjax(xhttp) {
     // itt a json content, benne a data változóban
     var userDatas = JSON.parse(xhttp.responseText);
     console.log(userDatas);
+    console.log(userDatas[0].city);
     /*
       Pár sorral lejebb majd ezt olvashatod:
       IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ!
@@ -24,6 +25,22 @@ function successAjax(xhttp) {
       Ha valemelyik függvényeteknek kell, akkor paraméterként adjátok át.
     */
     createTable(userDatas);
+    document.getElementById('bN').addEventListener("click", function () {
+        beforeNinety(userDatas);
+    });
+    document.getElementById('tO').addEventListener("click", function () {
+        threeOldest(userDatas);
+    });
+    document.getElementById('cOA').addEventListener("click", function () {
+        cities(userDatas);
+    });
+    document.getElementById('y2k').addEventListener("click", function () {
+        beforeY2K(userDatas);
+    });
+    document.getElementById('sort').addEventListener("click", function () {
+        stat(userDatas);
+    });
+
 }
 
 getData('JS/adminP/users.json', successAjax);
@@ -31,14 +48,140 @@ getData('JS/adminP/users.json', successAjax);
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
 
+//1990 előt születtek
+function beforeNinety(data) {
+    var elders = data.filter(function (i) {
+        var bday = new Date(i.birthdate);
+        if (bday.getFullYear() < 1990) {
+            return true;
+        }
+        return false;
+    });
+    tableBnHead();
+    tableBnCon(elders);
+}
+//Fejléc a 90 előtt szülötteknek
+function tableBnHead() {
+    var tableHead = '';
+    tableHead += `
+    <tr>
+        <td>Felhasználónév</td>
+    </tr>`
+    tableHead += '</thead>';
+    document.querySelector('#tablazatHead').innerHTML = tableHead;
+}
+//username a 90 előtt szülötteknek
+function tableBnCon(elders) {
+    var table = '';
+    for (var i = 0; i < elders.length; i++) {
+        table += `
+        <tr>
+             <td>${elders[i].username}</td>
+        </tr>`;
+    }
+    table += '</tbody>';
+    document.querySelector('#tablazat').innerHTML = table;
+}
+//tO NEM JÓ
+function threeOldest(data) {
+    var evek = [];
+    var elders = data.filter(function (i) {
+        var bday = new Date(i.birthdate);
+        var year = bday.getFullYear();
+        evek.push(year);
+    });
+    evek.sort(function (a, b) {
+        return a - b
+    });
+    console.log(evek);
+    tabletOHead()
+    tabletOCon(data)
+}
+//Fejléc a tO-nak
+function tabletOHead() {
+    var tableHead = '';
+    tableHead += `
+    <tr>
+        <td>Vezetéknév</td>
+        <td>Keresztknév</td>
+        <td>Születési dátum</td>
+    </tr>`
+    tableHead += '</thead>';
+    document.querySelector('#tablazatHead').innerHTML = tableHead;
+}
+// content a tO-nak
+function tabletOCon(userDatas) {
+    var table = '';
+    for (var i = 0; i < 3; i++) {
+        table += `
+        <tr>
+        <td>${userDatas[i].firstname}</td>
+        <td>${userDatas[i].lastname}</td>
+        <td>${userDatas[i].birthday}</td>
+        </tr>`;
+    }
+    table += '</tbody>';
+    document.querySelector('#tablazat').innerHTML = table;
+}
 
-function beforeNinety(userDatas) {
+//Városok Minden város neve, amelyben legalább ketten laknak, és mellette a lakosok száma
+//NEM JÓ
+function cities(data) {
+    for (var i in data) {
 
-    for (var i = 0; i < userDatas.length; i++) {}
+    } {
 
+    }
+    console.log(counts);
+}
+
+//Minden 2000 előtt született felhasználó, vagy nem Budapesti felhasználó vezeték-, kereszt- és felhasználóneve, email címe, telefonszáma. 
+function beforeY2K(data) {
+    var elders = data.filter(function (i) {
+        var bday = new Date(i.birthdate);
+        if (bday.getFullYear() < 2000 || data.city != 'Budapest') {
+            return true;
+        }
+        return false;
+    });
+    console.log(elders);
+    tableY2KHead()
+    tableY2KCon(elders)
+}
+
+//Fejléc y2k
+function tableY2KHead() {
+    var tableHead = '';
+    tableHead += `
+    <tr>
+        <td>Vezetéknév</td>
+        <td>Keresztnév</td>
+        <td>Felhasználónév</td>
+        <td>Email cím</td>
+        <td>Telefonszám</td>
+    </tr>`
+    tableHead += '</thead>';
+    document.querySelector('#tablazatHead').innerHTML = tableHead;
+}
+//content y2k
+function tableY2KCon(elders) {
+    var table = '';
+    for (var i = 0; i < elders.length; i++) {
+        table += `
+        <tr>
+             <td>${elders[i].firstname}</td>
+             <td>${elders[i].lastname}</td>
+             <td>${elders[i].username}</td>
+             <td>${elders[i].email}</td>
+             <td>${elders[i].phone}</td>
+        </tr>`;
+    }
+    table += '</tbody>';
+    document.querySelector('#tablazat').innerHTML = table;
 }
 
 
+//kiír minden contentet
 function createTable(userDatas) {
     var table = '';
     for (var i = 0; i < userDatas.length; i++) {
@@ -67,10 +210,10 @@ function createTable(userDatas) {
     document.querySelector('#tablazat').innerHTML = table;
     createHead()
 }
-
+//kód a fejléchez
 function createHead() {
-    var table = '';
-    table += `
+    var tableHead = '';
+    tableHead += `
 
     <tr>
         <td>Azonosító</td>
@@ -89,7 +232,26 @@ function createHead() {
         <td>Telefonszám</td>
     </tr>`
 
-    table += '</thead>';
-    document.querySelector('#tablazatHead').innerHTML = table;
+    tableHead += '</thead>';
+    document.querySelector('#tablazatHead').innerHTML = tableHead;
 }
-//<td>${jsonContent[i]["FIFA ranking"]} </td>
+
+function stat(userDatas) {
+    var date = new Date();
+    var yearNow = date.getFullYear();
+    var ossz = 0;
+    userDatas.sort(function (first, second) {
+        var korF = new Date(first.birthdate);
+        var korS = new Date(second.birthdate);
+        return korF - korS;
+    });
+    for (var i in userDatas) {
+        kor = new Date(userDatas[i].birthdate);
+        ossz += date.getFullYear() - kor.getFullYear();
+    }
+    var avg = ossz / userDatas.length;
+    document.getElementById('first').innerHTML = `A legidősebb: ${userDatas[0].username}, ${userDatas[0].birthdate}`;
+    document.getElementById('second').innerHTML = `A legfiatalabb: ${userDatas[userDatas.length - 1].username}, ${userDatas[userDatas.length - 1].birthdate}`;
+    document.getElementById('third').innerHTML = `Az össz életkor: ${ossz}`;
+    document.getElementById('fourth').innerHTML = `Az átlag életkor: ${avg}`;
+}
