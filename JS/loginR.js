6960e0a8f74345feeee30bfe211eb8026d71925e
Rekaert <36199853+Rekaert@users.noreply.github.com>
2018-03-11 23:39:34 +0100

    modal login

--- JS/loginR.js
+++ JS/loginR.js
@@ -41,42 +41,126 @@ var users = [{
 ];
 
 
-var button = document.querySelector('#button').value;
+setTimeout(function () {
+    $('#loginR').modal('show')
+}, 2000);
 
-var userName = document.querySelector('#userName').value;
+checkIn();
 
-var password = document.querySelector('#password').value;
+let loginAttempt = 0;
 
-var error = "Meg kell adnod a felhasználóneved és jelszavad!";
+function checkIn() {
 
-var forgot = document.querySelector('#forgotPassword');
+    $('.btn').click(function () {
+        let email = $('#userName').val();
+        let pass = $('#password').val();
+        //let findEmail = users[k].email;
+        //let foundPassword = users[k].password;
 
-var attempt = 0;
 
-button.addEventListener('click', function () {
-    if (userName == 0 || password == 0) {
-        document.querySelector('#error').innerHTML = error;
-    } else {
-        for (var i = 0; i < users.length; i++) {
-            if (users[i].email === userName && users[i].password === password) {
-                document.querySelector('#success').innerHTML = "Belépve: " + user[i].email;
-                window.location.href = 'adminR.html';
-            } else if (attempt > 4) {
-                document.querySelector('#error').innerHTML = "Háromszor is elrontottad az adataidat, 24 órára kitiltottunk!"
-                button.disabled = true;
+        if (email == '' || pass == '') {
+
+            $('.btn').before('<span id="error" style="color:red">Meg kell adnod a felhasználóneved és jelszavad</span>');
+        }
+
+
+        let found = users.find(function (elem) {
+            return elem.email === email && elem.password === pass;
+
+        });
+        (users[k])
+
+        for (let k in users) {
+
+            if (!found && loginAttempts < 4) {
+                loginAttempt += 1;
+                $('.btn').before('<span id="error" style="color:red">Hibás felhasználónév vagy jelszó</span>');
+                //$('#error').after(`Próbálkozások száma: ${loginAttempt}`); //document.querySelector(''#error).innerHTML = `Próbálkozások száma: ${loginAttempt}`;
+                $('#error').remove();
+
+            } else if (!found && loginAttempt > 3) {
+                $('#error').remove();
+                $('.btn').before('<span id="error" style="color:red">Háromszor is elrontottad az adataidat, 24 órára kitiltottunk</span>');
+                $('.btn').attr('class', 'disabled');
+
             } else {
-                error.innerHTML = "Hibás felhasználónév vagy jelszó";
-                attempt++;
+                $('#error').remove();
+                $('.btn').before(`<span id="success" style="color:green">Belépve: ${users[i].email}</span>`); //document.querySelector('.form-group').innerHTML = `Belépve: ${users[i].email}`;
+                loginAttempt = 0;
+                window.location.href = 'adminR.html';
+            }
+
+        }
+    });
+}
+
+/*
+
+function checkIn() {
+    $('.btn').click(function () {
+
+        let username = $('#userName').val();
+        let password = $('#password').val();
+        let spanElem = $('#error').text('');
+        let spanElem2 = $('#success').text('');
+        //let found = (users[i].email === username && users[i].password === password);
+        let loginAttempt = 0;
+
+
+
+        if (username == '' || password == '') {
+
+            $('.btn').before('<span id="error" style="color:red">Meg kell adnod a felhasználóneved és jelszavad</span>');
+            // spanElem += 'Meg kell adnod a felhasználóneved és jelszavad';
+
+        } else {
+            for (let i = 0; i < users.length; i++) {
+                // if (users[i].email !== username && users[i].password !== password) {
+
+                    $('.btn').before('<span id="error" style="color:red">Hibás felhasználónév vagy jelszó</span>'); //létrehozom az error idjú elemet, amit a spanElem változóba mentek el
+                    //spanElem += 'Hibás felhasználónév vagy jelszó';
+                    $('#error').after(`Próbálkozások száma: ${loginAttempt}`);
+                    $('#error').remove();
+                    loginAttempt += 1;
+
+                if (!found && loginAttempt > 3) {
+                    //spanElem = '';
+                    $('#error').remove();
+                    // spanElem += 'Háromszor is elrontottad az adataidat, 24 órára kitiltottunk';
+                    $('.btn').before('<span id="error" style="color:red">Háromszor is elrontottad az adataidat, 24 órára kitiltottunk</span>');
+                    $('.btn').attr('class', 'disabled');
+                    //$('.btn').off('click');
+                }
+                }
+
+                //if (users[i].email === username && users[i].password === password) {
+                else if (found) {
+                    $('#error').remove();
+                    $('.btn').before(`<span id="success" style="color:green">Belépve: ${users[i].email}</span>`);
+                    loginAttempt = 0;
+                    // spanElem2 += `Belépve: ${users[i].email}`;
+                    //window.open("adminR.html");
+                }
             }
         }
-    }
-});
+    });
+}
+*/
 
-forgot.addEventListener('click', function () {
-    var prompt = prompt('Adja meg e-mail címét');
-    if (prompt != "" && users.indexof(prompt) > -1) {
-        alert('Az új jelszó el lett küldve az e-mail címére');
-    } else {
-        alert('Nincs ilyen regisztrált felhasználó!');
+$('#forgot').click(function () {
+    let email = prompt('Kérem, adja meg e-mail címét: ');
+
+    for (let k in users) {
+        let found = users.find(function (elem) {
+            return elem.email === email;
+        });
+        if (!found) {
+            alert('Nincs ilyen regisztrált felhasználó');
+            break;
+        } else {
+            alert(`Az új jelszót elküldtük a ${users[k].email} címre`);
+            break;
+        }
+
     }
 });
